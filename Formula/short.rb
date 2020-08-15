4eedbcf8b2bb35a6f4fc964c3d140b10c42d3b34
Vincent Herlemont (bot) <vinentherl@leszeros.com>
2020-08-15 08:34:07 +0000

    release v0.3.4

--- Formula/short.rb
+++ Formula/short.rb
@@ -1,8 +1,8 @@
 class Short < Formula
   desc "Concise cli launcher / project manager using env files"
   homepage "https://github.com/vincent-herlemont/short"
-  url "https://api.github.com/repos/vincent-herlemont/short/tarball/v0.3.3"
-  sha256 "e2d07b06e65e651abae880a7612d806d7d042fb712c8151ecca668321c8e73ba"
+  url "https://github.com/vincent-herlemont/short/archive/v0.3.4.tar.gz"
+  sha256 "6c6ce5f3e3e3af1c7f3f802f3099fac8c21dfd68b14c8d3417d1e5c4407f040a"
   license ["Apache-2.0", "MIT"]
 
   depends_on "openssl" => :build
@@ -13,7 +13,7 @@ class Short < Formula
     cmd="cargo vendor"
     puts cmd
     IO.popen(cmd) { |io| while (line = io.gets) do puts line end }
-    cmd="cargo build --release --locked --frozen --features disabled_check_new_version"
+    cmd="cargo build --release --locked --frozen --no-default-features"
     puts cmd
     IO.popen(cmd) { |io| while (line = io.gets) do puts line end }
     bin.install "target/release/sht" => "sht"
