c8b41518aaed612eeb581f2ffd7391b0bf822ae6
Vincent Herlemont (bot) <vinentherl@leszeros.com>
2021-01-25 10:01:59 +0000

    release v0.3.6

--- Formula/short.rb
+++ Formula/short.rb
@@ -1,8 +1,8 @@
 class Short < Formula
   desc "Concise cli launcher / project manager using env files"
   homepage "https://github.com/vincent-herlemont/short"
-  url "https://github.com/vincent-herlemont/short/archive/v0.3.5.tar.gz"
-  sha256 "deb8016dce423ec873cca72096f6dc52be942b614a397b8cdda1aef967397c6c"
+  url "https://github.com/vincent-herlemont/short/archive/v0.3.6.tar.gz"
+  sha256 "1ef98ca8f8ee457d1bcbc96509894994c27911d1a92380d239e2c3992b834881"
   license ["Apache-2.0", "MIT"]
 
   depends_on "openssl" => :build
